35b5d4e398d8d4a36f81ffd27d2d48ad9390b086
aachikashukla06 <shuklaaachika21@gmail.com>
2026-06-07 22:58:06 +0530

    changes

--- FrontEnd/MYFIRSTREACTAPP/src/Pages/ContactUs.jsx
+++ FrontEnd/MYFIRSTREACTAPP/src/Pages/ContactUs.jsx
@@ -3,6 +3,7 @@ function ContactUs() {
         <>
          <div className="container d-flex justify-content-center align-items-center">
             <span className="text-primary fs-2"> Contact Us</span>
+            need help? contact us at 123-456-7890 or email us at support@example.com
             </div>
         </>
     );
--- FrontEnd/MYFIRSTREACTAPP/src/Pages/Product.jsx
+++ FrontEnd/MYFIRSTREACTAPP/src/Pages/Product.jsx
@@ -3,9 +3,14 @@ function Product() {
         <>
          <div className="container d-flex justify-content-center align-items-center">
             <span className="text-primary fs-2">Product</span>
+
+            product page
+            
             </div>
         </>
     );
 }
+
+
 
 export default Product;
