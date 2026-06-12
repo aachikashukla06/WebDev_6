37cf8171ebac3e20d5d96b3146a1eec97a2e91b3
aachikashukla06 <shuklaaachika21@gmail.com>
2026-06-12 18:18:49 +0530

    .

--- FrontEnd/MYFIRSTREACTAPP/src/App.jsx
+++ FrontEnd/MYFIRSTREACTAPP/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <BrowserRouter>
         <Header />
-
+      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
