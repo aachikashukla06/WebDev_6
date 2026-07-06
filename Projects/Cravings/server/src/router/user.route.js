a297aff802b500eebb9b33f8089e4104669b2b77
aachikashukla06 <shuklaaachika21@gmail.com>
2026-07-06 19:17:17 +0530

    implement profile picture upload and cloudinary integration in user profile

--- Projects/Cravings/server/src/router/user.route.js
+++ Projects/Cravings/server/src/router/user.route.js
@@ -1,11 +1,16 @@
 import express from "express";
 import multer from "multer";
 import { EditUserProfile } from "../controller/user.controller.js";
-import { AuthProtect } from "../middleware/auth.middleware.js";
+import { AuthProtect } from "../middleware/auth.middelware.js";
 
+const Upload = multer();
 const router = express.Router();
-const upload = multer( );
 
-router.put("/edit-profile", AuthProtect,upload.single("displayPicture") ,EditUserProfile);
+router.put(
+  "/edit-profile",
+  AuthProtect,
+  Upload.single("displayPic"),
+  EditUserProfile,
+);
 
 export default router;
